672e1b1b8680411057947c73606b68275919752f
Prashil1999 <prashilgupta99@gmail.com>
2020-11-08 19:25:47 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -2,9 +2,22 @@ var http = require("http");
 
 const httpServer = http.createServer(handleServer);
 
-
+let contactdata={
+    phone: '18602100000',
+    email: 'guestcaredominos@jublfood.com'
+    };
 function handleServer(req, res) {
+    if(req.url==="/welcome"){
+        res.write("Welcome to Dominos!");
+        res.end();
+    }else if(req.url==="/contact"){
+        res.write(JSON.stringify(contactdata));
+        res.end();
+    }else{
+        res.writeHead(404,"");
+        res.end();
+    }
   
 }
-
+httpServer.listen(8081,()=>{console.log("listen")});
 module.exports = httpServer;
