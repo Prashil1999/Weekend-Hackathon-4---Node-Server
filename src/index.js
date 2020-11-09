59ce3f028d346ac0682a7d614a150d363fdc77d6
Prashil1999 <prashilgupta99@gmail.com>
2020-11-09 23:16:25 +0530

    changes2

--- src/index.js
+++ src/index.js
@@ -8,9 +8,11 @@ let contactdata={
     };
 function handleServer(req, res) {
     if(req.url==="/welcome"){
+        res.writeHead(200,{ 'Content-Type': 'text/plain' });
         res.write("Welcome to Dominos!");
         res.end();
     }else if(req.url==="/contact"){
+        res.writeHead(200,{ 'Content-Type': 'application/json' });
         res.write(JSON.stringify(contactdata));
         res.end();
     }else{
